42064810fca1750fac34d6a96f366ffc49e474d5
Veit Vladimir <makoveev20122012@gmail.com>
2026-03-15 22:47:45 +0500

    Перезалив домашки из проекта T1

---
+++ src/Homework1_3.java
@@ -0,0 +1,12 @@
+public class Homework1_3 {
+    public static void main(String[] args) {
+        int N = 489;
+        int n1 = N/100;
+        int n2 = (N/10)%10;
+        int n3 = N%10;
+        System.out.println(n1);
+        System.out.println(n2);
+        System.out.println(n3);
+        System.out.println(n1+n2+n3);
+    }
+}
