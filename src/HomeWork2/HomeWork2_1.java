4352fea56f83669a06c8e391d179bf6149aead82
Veit Vladimir <makoveev20122012@gmail.com>
2026-03-22 12:48:27 +0500

    Домашка 2.1

--- src/HomeWork2/HomeWork2_1.java
+++ src/HomeWork2/HomeWork2_1.java
@@ -1,5 +1,16 @@
 package HomeWork2;
 
+import java.util.Scanner;
+
 public class HomeWork2_1 {
+    public static void main(String[] args) {
+        Scanner scaner = new Scanner(System.in);
+        int a = scaner.nextInt();
+        if (a % 2 == 0){
+            System.out.println("Good");
+        }else {
+            System.out.println("No good");
+        }
+    }
 
 }
