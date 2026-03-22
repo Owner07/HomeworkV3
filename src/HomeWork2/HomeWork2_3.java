c0cbabcf16b5fdf167b33b9de79e094487eb2450
Veit Vladimir <makoveev20122012@gmail.com>
2026-03-22 18:48:07 +0500

    Домашка 2.2

--- src/HomeWork2/HomeWork2_3.java
+++ src/HomeWork2/HomeWork2_3.java
@@ -1,4 +1,9 @@
 package HomeWork2;
 
 public class HomeWork2_3 {
+    public static void main(String[] args) {
+        for(int i = 10; i <= 20; i++) {
+            System.out.println(i*i);
+        }
+    }
 }
