2745fca6a32aba4e0d54dab9faebcd2d766b9a13
Veit Vladimir <makoveev20122012@gmail.com>
2026-04-03 21:54:04 +0500

    HW_5_2

--- src/HomeWork5/two/Rectangle.java
+++ src/HomeWork5/two/Rectangle.java
@@ -1,11 +1,15 @@
 package HomeWork5.two;
 
 public class Rectangle extends Figure{
-    public void perimeter(){
+    public Rectangle(int side, int side2, int side3, int side4) {
+        super(side, side2, side3, side4);
+    }
 
+    public void perimeter(){
+        System.out.println("Периметр прямоугольника = " + ((side + side2) * 2));
     }
 
     public void square(){
-
+        System.out.println("Площадь прямоугольника = " +(side * side2));
     }
 }
