3426e64b59c9399e5818cffc923353993e1657f6
Veit Vladimir <makoveev20122012@gmail.com>
2026-04-03 21:35:40 +0500

    HW_5_2

--- src/HomeWork5/two/Triangle.java
+++ src/HomeWork5/two/Triangle.java
@@ -1,11 +1,15 @@
 package HomeWork5.two;
 
 public class Triangle extends Figure{
-    public void perimeter(){
+    public Triangle(int side, int side2, int side3, int side4) {
+        super(side, side2, side3, side4);
+    }
 
+    public void perimeter(){
+        System.out.println("Периметр треугольника = " + (side + side2 + side3) + "см");
     }
 
     public void square(){
-
+        System.out.println("Площадь треугольник = " + side * (side4 / 2) + "см");
     }
 }
