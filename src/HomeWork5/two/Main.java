541ac32b032e67fa4cfd1b5028634e50476aaf8f
Veit Vladimir <makoveev20122012@gmail.com>
2026-04-04 10:58:03 +0500

    HW_5_2

--- src/HomeWork5/two/Main.java
+++ src/HomeWork5/two/Main.java
@@ -1,16 +1,19 @@
 package HomeWork5.two;
 
-import java.awt.Rectangle;
-
 public class Main {
     public static void main(String[] args) {
         Circle circle = new Circle(15);
-        Rectangle rectangle = new Rectangle(10, 20);
+        Rectangle rectangle = new Rectangle(10, 20, 30,40);
         Triangle triangle = new Triangle(10, 20, 30, 40);
 
         circle.perimeter();
         circle.square();
 
-        rectangle.
+        triangle.perimeter();
+        triangle.square();
+
+        rectangle.perimeter();
+        rectangle.square();
+
     }
 }
