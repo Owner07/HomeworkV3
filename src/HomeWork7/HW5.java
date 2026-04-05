ed761536bc29da0a4c1b4017564ce1e1275b3ae2
Veit Vladimir <makoveev20122012@gmail.com>
2026-04-05 20:15:10 +0500

    HW 7_5

--- src/HomeWork7/HW5.java
+++ src/HomeWork7/HW5.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class HW5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите слово: ");
+        System.out.println("Введите слово:  ");
         String word = scanner.nextLine();
         StringBuilder res = new StringBuilder();
         for (int i = 0; i < word.length(); i++) {
