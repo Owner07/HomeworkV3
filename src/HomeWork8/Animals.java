040151815f9e009771e1e4f5462e545a80a941be
Veit Vladimir <makoveev20122012@gmail.com>
2026-04-09 00:34:14 +0500

    HW 7_5

--- src/HomeWork8/Animals.java
+++ src/HomeWork8/Animals.java
@@ -1,4 +1,13 @@
 package HomeWork8;
 
+import java.util.LinkedList;
+
 public class Animals {
+    LinkedList<String> animals = new LinkedList<>();
+    public void addAnimal(){
+        animals.addFirst();
+    }
+    public void delAnimal(){
+        animals.removeLast();
+    }
 }
