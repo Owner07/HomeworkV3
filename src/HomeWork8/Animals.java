9d418ccf2becc5bc5ae46df418638633bda30197
Veit Vladimir <makoveev20122012@gmail.com>
2026-04-10 20:07:16 +0500

    HW 8_2

--- src/HomeWork8/Animals.java
+++ src/HomeWork8/Animals.java
@@ -4,10 +4,20 @@ import java.util.LinkedList;
 
 public class Animals {
     LinkedList<String> animals = new LinkedList<>();
-    public void addAnimal(){
-        animals.addFirst();
+
+    public void addAnimal(String animal){
+        animals.addFirst(animal);
     }
+
     public void delAnimal(){
-        animals.removeLast();
+        if(!animals.isEmpty()) {
+            animals.removeLast();
+        } else {
+            System.out.println("Список пуст, нечего удалять!");
+        }
+    }
+
+    public void showAnimals() {
+        System.out.println("Список животных: " + animals);
     }
 }
--- src/HomeWork8/Main.java
+++ src/HomeWork8/Main.java
@@ -1,4 +1,29 @@
 package HomeWork8;
 
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Animals animalist = new Animals();
+        System.out.println("Введите 1 для добавления животного, 2 для удаления: ");
+        int a = scanner.nextInt();
+        scanner.nextLine();
+        animalist.addAnimal("Кошка");
+        animalist.addAnimal("Собака");
+        animalist.addAnimal("Попугай");
+        if(a == 1){
+            System.out.println("Введите название животного");
+            String animal = scanner.nextLine();
+            animalist.addAnimal(animal);
+            animalist.showAnimals();
+        }else if (a == 2){
+            animalist.showAnimals();
+            animalist.delAnimal();
+            animalist.showAnimals();
+        }else {
+            System.out.println("Не верный код");
+        }
+    }
 }
